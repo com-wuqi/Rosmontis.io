6b8077340741a2f2d9b221de2ebb8f710d831f2b
com-wuqi <2025214516@ecut.edu.cn>
2026-02-27 14:33:57 +0800

    fix .env.prod add 音乐质量配置
    Signed-off-by: com-wuqi <2025214516@ecut.edu.cn>

--- src/plugins/yaohud/config.py
+++ src/plugins/yaohud/config.py
@@ -11,8 +11,9 @@ class ScopedConfig(BaseModel):
     upload_ws_url: str  # 上传 url
     upload_ws_token: str  # token
     wyvip_cookie: str  # 暂时不使用
+    wyvip_level: str  # 音质 , 参考 get_netease_music()
     qqmusic_cookie: str  # 暂时不使用
-
+    qqmusic_level: str  # size 定义歌曲音质，当type=url时有效，留空默认m4a试听音质，可选参数：mp3:普通音质、hq:高品质、sq:无损、hires:HiRes音质
 
 class Config(BaseModel):
     """插件主配置，包含作用域"""
